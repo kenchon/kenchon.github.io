7c3598d1bfac2e27d49f4f811140503e11cef65d
kenmikanmi <kenh5526@gmail.com>
2020-04-08 02:12:50 +0900

    feat(style): Modify quoting style

---
+++ src/@narative/gatsby-theme-novela/components/Blockquote/Blockquote.tsx
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+import mediaqueries from "@styles/media";
+
+const Blockquote = styled.blockquote`
+  transition: ${p => p.theme.colorModeTransition};
+  margin: 15px auto 50px;
+  color: ${p => p.theme.colors.articleText};
+  font-family: ${p => p.theme.fonts.serif};
+
+  padding: 30px 30px 30px 30px;
+  font-style: italic;
+  background-color: #f2f2f2;
+  foreground-color: #3d3d3d;
+  border-radius: 5px;
+
+  ${mediaqueries.tablet`
+    margin: 10px auto 35px;
+  `};
+
+  & > p {
+    font-family: ${p => p.theme.fonts.serif};
+    // max-width: 880px !important;
+    // padding-right: 100px;
+    padding-bottom: 0;
+    width: 100%;
+    margin: 0 auto;
+    // font-size: 36px;
+    line-height: 1.32;
+    // font-weight: bold;
+
+    ${mediaqueries.tablet`
+      font-size: 26px;
+      padding: 0 180px;
+    `};
+
+    ${mediaqueries.phablet`
+      font-size: 36px;
+      padding: 0 20px 0 40px;
+    `};
+  }
+`;
+
+export default Blockquote;
