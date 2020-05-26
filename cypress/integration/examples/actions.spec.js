0ea9ec6689f5d4ed218cf0425c874d875662d856
kenmikanmi <kenh5526@gmail.com>
2020-05-26 23:24:13 +0900

    Fix test code

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -8,6 +8,6 @@ context('Actions', () => {
   // トップのアイコンをクリックすると，トップページにいく。
   it('Click icon of the top left to visit toppage', () => {
     cy.get('a[href="/blog"]').click()
-    cy.url().should('match', /\/$/)
+    cy.url().should('match', /\/blog$/)
   })
 })
