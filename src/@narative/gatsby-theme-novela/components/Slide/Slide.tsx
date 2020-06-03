504f44bfecef94a1d187883a4d1dc371b5ba642e
kenmikanmi <kenh5526@gmail.com>
2020-06-03 10:46:45 +0900

    Add slide style

--- src/@narative/gatsby-theme-novela/components/Slide/Slide.tsx
+++ src/@narative/gatsby-theme-novela/components/Slide/Slide.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const IFrame = styled.iframe`
-    max-width: 507px;
+    max-width: 780px;
     max-height: 586px;
     top: 0;
-    width: 55%;
-    height: 55%;
+    width: 70%;
+    height: 70%;
     // position: relative;
     // text-align: center;
     border: 0;
+    top: 0;
+    left: 0;
+
 `
 
 const Frame = styled.div`
@@ -21,7 +24,8 @@ const Frame = styled.div`
     // position: relative;
     padding-bottom: 74.9296%;
     text-align: center;
-    margin-bottom: -250px;
+    // margin-bottom: -250px;
+    
 `
 
 const Slide = ( props ) => {
