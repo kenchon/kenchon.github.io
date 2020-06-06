a634455828754c74f88e6b0bdc30253f1608cdfe
kenmikanmi <kenh5526@gmail.com>
2020-06-06 22:30:28 +0900

    Modify width of hero image

--- src/@narative/gatsby-theme-novela/sections/article/Article.Hero.tsx
+++ src/@narative/gatsby-theme-novela/sections/article/Article.Hero.tsx
@@ -166,7 +166,7 @@ const HeroImage = styled.div`
   position: relative;
   z-index: 1;
   width: 100%;
-  max-width: 944px;
+  max-width: 780px;
   overflow: hidden;
   margin: 0 auto;
   box-shadow: 0 30px 60px -10px rgba(0, 0, 0, 0.2),
