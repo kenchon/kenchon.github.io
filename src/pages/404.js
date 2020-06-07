ea394033a01df27275b4ae915e58a1375f203727
kenmikanmi <kenh5526@gmail.com>
2020-06-07 22:17:42 +0900

    Update article and 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,6 +5,7 @@ import Section from "@narative/gatsby-theme-novela/src/components/Section";
 import SEO from "@narative/gatsby-theme-novela/src/components/SEO";
 import Headings from "@narative/gatsby-theme-novela/src/components/Headings";
 import Emphasis from "../@narative/gatsby-theme-novela/components/Emphasis/Emphasis";
+import { Link } from "gatsby"
 
 function NotFoundPage() {
   return (
@@ -16,7 +17,7 @@ function NotFoundPage() {
           <div style={{marginTop: "30px"}}></div>
           <img width="100%" src="https://lh3.googleusercontent.com/Vl8oKfNwRSdk5w-zpnMIXqh7dy4CWH6wMNQtzEfBtwzSyS2ImDulAjTSlaCJ_GQwZp5XNe0GoyNHB1rdWbF42_ShxvyxbmM6Kk1hIgf_xuXgesjR8D5Kch0z9JfRSfeq-rBI-zDFZxk" alt="I'm sorry!"></img>
           <div style={{marginTop: "30px"}}></div>
-          <Headings.h1>Click <a href="/">Here</a> To Get It Back!</Headings.h1>
+          <Headings.h1>Click <Link to="/">Here</Link> To Get It Back!</Headings.h1>
         </div>
       </Section>
     </Layout>
