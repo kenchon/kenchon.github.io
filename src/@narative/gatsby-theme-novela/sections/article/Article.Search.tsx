1d8f7325fb73f0db79549148a15f2e7bb64a30d4
kenchon <kenh5526@gmail.com>
2020-10-26 22:14:31 +0900

    feat: Add Search box

--- src/@narative/gatsby-theme-novela/sections/article/Article.Search.tsx
+++ src/@narative/gatsby-theme-novela/sections/article/Article.Search.tsx
@@ -9,15 +9,26 @@ const Search: React.FC = (props) => {
     return (
         <div>
             <StyledSearch>
+                <h1>サイト内検索</h1>
                 <script type="module" src="https://cse.google.com/cse.js?cx=210b2fa21846e482e"></script>
-                <div className="gcse-search"></div>
+                <div id="search-container" className="gcse-search"></div>
             </StyledSearch>
         </div>
     )
 }
 
 const StyledSearch = styled.div`
-    z-index: 1000;
+    h1 {
+        padding: 10px 10px 10px 10px;
+    }
+    div {
+        border-radius: 8px;
+        border-color: gray;
+        border-width: 2px;
+    }
+    padding: 0 50px 0 50px;
+    width: 100%;
+    z-index: 999;
     position: absolute;
     margin-bottom: 30px;
 `
